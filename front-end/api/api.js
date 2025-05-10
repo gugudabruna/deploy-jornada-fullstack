e18cf14e611f8e8b3b9f9e328341855d5ab737be
Márcia <Marcia@MacBook-Pro-de-Everton.local>
2025-05-10 00:22:44 -0300

    ajustes para o deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,14 @@
 // Fetch ou Axios
+import "dotenv/config";
 import axios from "axios";
 
+const { NODE_ENV } = process.env;
+
 //const URL = "http://localhost:3005";
-const URL = "https://deploy-jornada-full-stack.onrender.com/api";
+const URL =
+  NODE_ENV === "development"
+    ? "https://deploy-jornada-full-stack.onrender.com/api"
+    : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
